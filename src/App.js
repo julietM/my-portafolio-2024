2eeaef9f160b39d2b2ded1b8d4012e15883fc9c7
juliet@21 <julietyartes2@gmail.com>
2024-01-24 14:37:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        soy persona sorda.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         soy julieta 😎🔥
         </a>
       </header>
     </div>
